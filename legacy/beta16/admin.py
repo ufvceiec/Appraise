9df490fd1c3dbc877d9cd4ae8c727627b7feff14
Angel Melchor <angelmelchorsanchez@gmail.com>
2020-05-28 11:23:36 +0200

    deleted unnecessary files

--- legacy/beta16/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from appraise.beta16.models import AbsoluteScoringTask, AbsoluteScoringData
-from appraise.beta16.models import MetaData
-
-admin.site.register(AbsoluteScoringTask)
-admin.site.register(AbsoluteScoringData)
-admin.site.register(MetaData)
--- legacy/evaluation/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Project: Appraise evaluation system
- Author: Christian Federmann <cfedermann@gmail.com>
-"""
